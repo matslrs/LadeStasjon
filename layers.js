cb15d05d90358f3ec7713cad1dcac991075d8b33
mala15 <mats@ecc.no>
2016-03-30 11:46:51 +0200

    test for å sjekke om fylker.geojson virker

--- layers.js
+++ layers.js
@@ -145,7 +145,7 @@ function loadFylkeGeoJSONs(map) {
 	//creates and empty GeoJSON test Layer
 	var FylkeGeoJLayer = L.geoJson();
 
-	$.getJSON('GeoJSON/fylker.geoJSON', function(data) {
+	$.getJSON('GeoJSON/fylker.geojson', function(data) {
 		FylkeGeoJLayer.addData(data);
 	});
 
