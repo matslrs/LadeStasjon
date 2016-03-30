452b915d3a1aa6e178c5280dd17e8e630e74b527
mala15 <mats@ecc.no>
2016-03-30 11:33:00 +0200

    oppdatert kart1 med de nye variablene i layers

--- layers.js
+++ layers.js
@@ -53,6 +53,8 @@ function setupOverlayLayers(map) {
 		}
 	}
 	
+
+
 	//loads locally stored geoJSONs
 	if(useFylkeGeoJsonData) {
 		var FylkeGeoJLayer = loadFylkeGeoJSONs(map);
@@ -67,11 +69,20 @@ function setupOverlayLayers(map) {
 		overlayMaps["Grunnkretser 100mb"] = GrunnkretsGeoJLayer;
 	}
 
+
+
 	//gets JSON data from difi
 	if(useHelseStasjonData ) {
-		var helseStasjonDifi = difiDataSett(map);
-		overlayMaps["Helsestasjon difi"] = helseStasjonDifi;
+		var helseStasjonDifi = difiHelsestasjon(map);
+		overlayMaps["Helsestasjon"] = helseStasjonDifi;
 	}
+	if(useBarnehageData ) {
+		var barnehageDifi = difiBarnehage(map);
+		overlayMaps["Barnehage"] = barnehageDifi;
+	}
+
+
+
 	//manuelt lagt til GeoJSON data
 	if(useManualGeoData ) {
 		var myGeoJLayer_manuelt = initiateGeojsonManuelt(map);
@@ -128,59 +139,6 @@ function loadDrawAndMaplyticDB(map) {
 	return drawnItems;
 }
 
-//bug: legges ikke inn i myGeoLayer
-function initiateAndGetGeojsonData(map) {
-	//creates and empty GeoJSON Layer
-	var myGeoJLayer = L.geoJson();
-	//url til GeoJSON data
-	var url = 'https://mats.maplytic.no/table/test.geojson';
-	//henter data 
-	$.get(url, function(data) {
-
-		myGeoJLayer = L.geoJson(data, {
-
-	    	onEachFeature: function (feature, layer) {
-	    		layer.bindPopup("Gid: " + feature.properties.gid + "<br>" + "Geometry Type: " + feature.geometry.type);  
-		    }
-
-		}).addTo(map);
-
-		
-
-	});	
-
-	return myGeoJLayer;
-}
-
-//bug: får ikke opp popup med informasjon om feature
-function initiateAndGetGeojsonData2(map) {
-	//creates and empty GeoJSON Layer
-	var myGeoJLayer = L.geoJson();
-
-	//url til GeoJSON data 
-	var url = 'https://mats.maplytic.no/table/test.geojson';
-	//henter data 
-
-	$.getJSON(url, function(data) {
-
-    function onEachFeature(feature, layer) {
-  
-        layer.bindPopup("Gid: " + feature.properties.gid + "<br>" + "Geometry Type: " + feature.geometry.type);
-    } 
-
-
-
-    myGeoJLayer.addData(data, {
-      onEachFeature: onEachFeature
-    });
-
-    myGeoJLayer.addTo(map);
-  });	
-
-
-	return myGeoJLayer;
-}
-
 //local geoJSON data
 function loadFylkeGeoJSONs(map) {
 
@@ -219,6 +177,163 @@ function loadGrunnkretsGeoJSONs(map) {
 
 }
 
+
+
+function difiHelsestasjon(map) {
+	
+	//creates and empty GeoJSON Layer
+	var helsestasjonGroup = L.layerGroup().addTo(map);
+	
+	//var difiData = null;
+	var helsestasjoner = [];	
+	//url til JSON data 
+	var url = 'https://hotell.difi.no/api/json/stavanger/helsestasjoner?';
+	//henter data 
+	$.get(url, function(data) {
+		//var difiData = JSON.parse(data);
+		var difiData = data;
+		
+		for (i = 0; i < difiData.entries.length; i++) {
+			
+			//Finner data som skal brukes
+			lengdeGrad = difiData.entries[i].lengdegrad;
+			breddeGrad = difiData.entries[i].breddegrad;
+			tittel = difiData.entries[i].navn;
+			alt = difiData.entries[i].adresse;
+			
+			//Lager marker for helsestasjonen
+			helsestasjoner[i] = L.marker([breddeGrad, lengdeGrad]);
+			helsestasjoner[i].title = tittel;
+			helsestasjoner[i].alt = alt;
+			
+			helsestasjoner[i].bindPopup("<strong>" + tittel +  "</strong> <br>"+ alt);
+			
+			helsestasjonGroup.addLayer( helsestasjoner[i] );
+		}
+	});
+
+	return helsestasjonGroup;
+}
+
+function difiBarnehage(map) {
+	
+	//creates and empty GeoJSON Layer
+	var barnehageGroup = L.layerGroup().addTo(map);
+	
+	//var difiData = null;
+	var barnehager = [];	
+	//url til JSON data 
+	var url = 'https://hotell.difi.no/api/json/stavanger/barnehager?';
+	//henter data 
+	$.get(url, function(data) {
+		//var difiData = JSON.parse(data);
+		var difiData = data;
+		
+		for (i = 0; i < difiData.entries.length; i++) {
+			
+			//Finner data som skal brukes
+			lengdeGrad = difiData.entries[i].lengdegrad;
+			breddeGrad = difiData.entries[i].breddegrad;
+			tittel = difiData.entries[i].navn;
+			alt = difiData.entries[i].adresse;
+			
+			//Lager marker for helsestasjonen
+			barnehager[i] = L.marker([breddeGrad, lengdeGrad]);
+			barnehager[i].title = tittel;
+			barnehager[i].alt = alt;
+			
+			barnehager[i].bindPopup("<strong> Barnehage: </strong> <br>" + tittel +  " <br>"+ alt);
+			
+			barnehageGroup.addLayer( barnehager[i] );
+		}
+	});
+
+	return barnehageGroup;
+}
+
+
+///////////////////
+/////Test ting////
+/////////////////
+
+
+//bug: legges ikke inn i myGeoLayer
+function initiateAndGetGeojsonData(map) {
+	//creates and empty GeoJSON Layer
+	var myGeoJLayer = L.geoJson();
+	//url til GeoJSON data
+	var url = 'https://mats.maplytic.no/table/test.geojson';
+	//henter data 
+	$.get(url, function(data) {
+
+		myGeoJLayer = L.geoJson(data, {
+
+	    	onEachFeature: function (feature, layer) {
+	    		layer.bindPopup("Gid: " + feature.properties.gid + "<br>" + "Geometry Type: " + feature.geometry.type);  
+		    }
+
+		}).addTo(map);
+
+		
+
+	});	
+
+	return myGeoJLayer;
+}
+//bug: får ikke opp popup med informasjon om feature
+function initiateAndGetGeojsonData2(map) {
+	//creates and empty GeoJSON Layer
+	var myGeoJLayer = L.geoJson();
+
+	//url til GeoJSON data 
+	var url = 'https://mats.maplytic.no/table/test.geojson';
+	//henter data 
+
+	$.getJSON(url, function(data) {
+
+    function onEachFeature(feature, layer) {
+  
+        layer.bindPopup("Gid: " + feature.properties.gid + "<br>" + "Geometry Type: " + feature.geometry.type);
+    } 
+
+
+
+    myGeoJLayer.addData(data, {
+      onEachFeature: onEachFeature
+    });
+
+    myGeoJLayer.addTo(map);
+  });	
+
+
+	return myGeoJLayer;
+}
+function testPopups(map) {
+	//POPUPS
+	var marker = L.marker([58.85, 5.74]);
+
+	var circle = L.circle([58.8534, 5.7207], 200, {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5
+	});
+
+	var polygon = L.polygon([
+		[58.85, 5.73],
+		[58.855, 5.74],
+		[58.859, 5.735]
+	]);
+
+	//more popup stuff
+	marker.bindPopup("Jeg er midti byen!");
+	circle.bindPopup("Midt i gata");
+	polygon.bindPopup("Langs jernbanen");
+
+	//adds popups to layer group
+	var markers_group = L.layerGroup([marker, circle, polygon]).addTo(map);
+
+	return markers_group;
+}
 //noen GeoJSON data som er manuelt lagt inn
 function initiateGeojsonManuelt(map) {
 	//defines a GeoJSON Feature
@@ -262,70 +377,4 @@ function initiateGeojsonManuelt(map) {
 
 
 	return myGeoJLayer_manuelt;
-
-}
-
-function difiDataSett(map) {
-	
-	//creates and empty GeoJSON Layer
-	var helsestasjonGroup = L.layerGroup().addTo(map);
-	
-	//var difiData = null;
-	var helsestasjoner = [];	
-	//url til JSON data 
-	var url = 'https://hotell.difi.no/api/json/stavanger/helsestasjoner?';
-	//henter data 
-	$.get(url, function(data) {
-		//var difiData = JSON.parse(data);
-		var difiData = data;
-		
-		for (i = 0; i < difiData.entries.length; i++) {
-			
-			//Finner data som skal brukes
-			lengdeGrad = difiData.entries[i].lengdegrad;
-			breddeGrad = difiData.entries[i].breddegrad;
-			tittel = difiData.entries[i].navn;
-			alt = difiData.entries[i].adresse;
-			
-			//Lager marker for helsestasjonen
-			helsestasjoner[i] = L.marker([breddeGrad, lengdeGrad]);
-			helsestasjoner[i].title = tittel;
-			helsestasjoner[i].alt = alt;
-			
-			helsestasjoner[i].bindPopup("<strong>" + tittel +  "</strong> <br>"+ alt);
-			
-			helsestasjonGroup.addLayer( helsestasjoner[i] );
-		}
-		
-		
-	});
-
-	return helsestasjonGroup;
-}
-
-function testPopups(map) {
-	//POPUPS
-	var marker = L.marker([58.85, 5.74]);
-
-	var circle = L.circle([58.8534, 5.7207], 200, {
-		color: 'red',
-		fillColor: '#f03',
-		fillOpacity: 0.5
-	});
-
-	var polygon = L.polygon([
-		[58.85, 5.73],
-		[58.855, 5.74],
-		[58.859, 5.735]
-	]);
-
-	//more popup stuff
-	marker.bindPopup("Jeg er midti byen!");
-	circle.bindPopup("Midt i gata");
-	polygon.bindPopup("Langs jernbanen");
-
-	//adds popups to layer group
-	var markers_group = L.layerGroup([marker, circle, polygon]).addTo(map);
-
-	return markers_group;
 }
