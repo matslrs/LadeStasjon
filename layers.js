1777d72ecb7f05a0f021a9c196236f172d2dc85e
mala15 <mats@ecc.no>
2016-07-19 07:42:44 +0200

    more of the same

--- layers.js
+++ layers.js
@@ -806,41 +806,41 @@ function difiBomstasjon(map) {
 	return bomstasjonGroup;
 }
 
-function bringPickupPointsBackup(map) {
+function bringPickupPoints(map) {
 	
 	//creates and empty subgroup
 	var pickupGroup = L.featureGroup.subGroup(parentCluster);
 
-
-	//url til JSON data 
-	var url = 'https://api.bring.com/pickuppoint/api/pickuppoint/no/all.json';
-	//henter data 
-	$.get(url, function(data) {
-		var bringData = JSON.parse(data);
-		//var bringData = data;
+	jQuery.ajax({
+		type: 'GET',
+		url: 'https://api.bring.com/pickuppoint/api/pickuppoint/no/all.json',
+		success: function(data){
+			var bringData = data;
 		
-		for (i = 0; i < bringData.pickupPoint.length; i++) {
-			
-			//Finner data som skal brukes
-			var lengdeGrad = bringData.pickupPoint[i].longitude;
-			var breddeGrad = bringData.pickupPoint[i].latitude;
-			var tittel = bringData.pickupPoint[i].name;
-			var adress = bringData.pickupPoint[i].address;
-			var beskrivelse = bringData.pickupPoint[i].locationDescription;
-			var open = bringData.pickupPoint[i].openingHoursNorwegian;
-			
-			var marker = L.marker([breddeGrad, lengdeGrad], {icon: pickupBox});
-			marker.bindPopup("<strong>Pickup Point:</strong> <br>" + tittel + "<br> Adress: " + alt + "<br> Beskrivelse: " + beskrivelse + "<br> Åpen: " + open );
+			for (i = 0; i < bringData.pickupPoint.length; i++) {
+				
+				//Finner data som skal brukes
+				var lengdeGrad = bringData.pickupPoint[i].longitude;
+				var breddeGrad = bringData.pickupPoint[i].latitude;
+				var tittel = bringData.pickupPoint[i].name;
+				var adress = bringData.pickupPoint[i].address;
+				var beskrivelse = bringData.pickupPoint[i].locationDescription;
+				var open = bringData.pickupPoint[i].openingHoursNorwegian;
+				
+				var marker = L.marker([breddeGrad, lengdeGrad], {icon: pickupBox});
+				marker.bindPopup("<strong>Pickup Point:</strong> <br>" + tittel + "<br> Adress: " + alt + "<br> Beskrivelse: " + beskrivelse + "<br> Åpen: " + open );
 
-			//adds marker to sub group
-			pickupGroup.addLayer(marker);
-		}
+				//adds marker to sub group
+				pickupGroup.addLayer(marker);
+			}
+		},
+		dataType: 'json'
 	});
 
 	return pickupGroup;
 }
 
-function bringPickupPoints(map) {
+function bringPickupPoints2(map) {
 	
 	//creates and empty subgroup
 	var pickupGroup = L.featureGroup.subGroup(parentCluster);
